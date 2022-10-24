4decbd7297318c03d3265f2e165bbd45154de997
wwwkr <wwwkr@KAPIL>
2022-10-24 16:14:52 +0530

    first program

---
+++ src/main/java/Sauce_Demo_TestClass/Githubpractice.java
@@ -0,0 +1,12 @@
+package Sauce_Demo_TestClass;
+
+import org.testng.annotations.Test;
+
+public class Githubpractice extends Test_BAse_Class_SauceDemo
+{
+ @Test
+ public void Verifyproduct()
+ {
+	 System.out.println("product verified");
+ }
+}
